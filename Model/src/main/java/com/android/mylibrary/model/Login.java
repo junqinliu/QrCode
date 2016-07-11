34644292db7b92e51c1a99457e42682ccd1e7caf
junqinliu <275063613@qq.com>
2016-07-11 11:17:35 +0800

    集成sharesdk第三方分享

---
+++ Model/src/main/java/com/android/mylibrary/model/Login.java
@@ -0,0 +1,158 @@
+package com.android.mylibrary.model;
+
+/**
+ * Created by liujunqin on 2016/6/29.
+ */
+public class Login {
+
+
+    /**
+     * 头像
+     */
+    private String imgUrl;
+    /**
+     * Pin编号
+     */
+    private String userPin;
+    /**
+     * Sim卡编号
+     */
+    private String userSim;
+    /**
+     * 手机类型
+     */
+    private String phoneType;
+
+    /**
+     * 用户id
+     */
+    private String userId;
+    /**
+     * 机构号
+     */
+    private String orgId;
+    /**
+     * 机构名称
+     */
+    private String orgName;
+    /**
+     * 用户编号
+     */
+    private String userCode;
+    /**
+     * 用户电话
+     */
+    private String userPhone;
+    /**
+     * 用户姓名
+     */
+    private String userName;
+    /**
+     * 签到状态
+     */
+    private String signinStatus;
+    /**
+     * 时间标识
+     */
+    private String token;
+    /**
+     * 请假开始时间
+     */
+    private String leaveStart;
+    /**
+     * 请假结束时间
+     */
+    private String leaveEnd;
+
+
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getUserPin() {
+        return userPin;
+    }
+    public void setUserPin(String userPin) {
+        this.userPin = userPin;
+    }
+    public String getUserSim() {
+        return userSim;
+    }
+    public void setUserSim(String userSim) {
+        this.userSim = userSim;
+    }
+    public String getPhoneType() {
+        return phoneType;
+    }
+    public void setPhoneType(String phoneType) {
+        this.phoneType = phoneType;
+    }
+
+
+
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public String getOrgId() {
+        return orgId;
+    }
+    public void setOrgId(String orgId) {
+        this.orgId = orgId;
+    }
+    public String getOrgName() {
+        return orgName;
+    }
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+    public String getUserCode() {
+        return userCode;
+    }
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+    public String getUserPhone() {
+        return userPhone;
+    }
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getSigninStatus() {
+        return signinStatus;
+    }
+    public void setSigninStatus(String signinStatus) {
+        this.signinStatus = signinStatus;
+    }
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public String getLeaveStart() {
+        return leaveStart;
+    }
+    public void setLeaveStart(String leaveStart) {
+        this.leaveStart = leaveStart;
+    }
+    public String getLeaveEnd() {
+        return leaveEnd;
+    }
+    public void setLeaveEnd(String leaveEnd) {
+        this.leaveEnd = leaveEnd;
+    }
+
+}
