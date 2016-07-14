0082ff4f386b0248cc5829597ae7ee95f563bd2d
junqinliu <275063613@qq.com>
2016-07-14 21:24:55 +0800

    实现登录接口

--- app/src/main/java/com/android/qrcode/LoginActivity.java
+++ app/src/main/java/com/android/qrcode/LoginActivity.java
@@ -6,17 +6,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import com.android.base.BaseAppCompatActivity;
 import com.android.constant.Constants;
 import com.android.mylibrary.model.Login;
+import com.android.utils.HttpUtil;
 import com.google.gson.Gson;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 
 import butterknife.Bind;
 
-public class LoginActivity extends BaseAppCompatActivity implements View.OnClickListener{
-
+public class LoginActivity extends BaseAppCompatActivity implements View.OnClickListener {
 
 
     @Bind(R.id.btn_login)
@@ -65,77 +72,95 @@ public class LoginActivity extends BaseAppCompatActivity implements View.OnClick
     @Override
     public void onClick(View view) {
 
-      switch (view.getId()){
+        switch (view.getId()) {
 
-        //登录按钮
-        case R.id.btn_login:
-            /*Login login = new Login();
-            login.setUserCode("liujq");
-            login.setUserPhone("15522503900");
-            login.setPhoneType("HM 1SW");
-            login.setUserSim("89860110421307064473");
-            login.setUserPin("865624021665253");
-            Gson gson = new Gson();
-            String jsonstr = gson.toJson(login);
-            RequestParams params = new RequestParams(Constants.HOST+Constants.Login);
-            params.addBodyParameter("loginRequest", jsonstr);
+            //登录按钮
+            case R.id.btn_login:
 
-            x.http().post(params, new Callback.CommonCallback<String>() {
-                @Override
-                public void onSuccess(String result) {
-                    Toast.makeText(x.app(), result, Toast.LENGTH_LONG).show();
+                RequestParams params = new RequestParams();
+                params.put("phone", "15522503900");
+                params.put("password", "liujq");
+
+                HttpUtil.post("http://120.76.202.60:8080/microcard/v0.1/user/login", params, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onStart() {
+                        super.onStart();
+
+
+                    }
+
+
+                    @Override
+                    public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody) {
+
+                        if (responseBody != null) {
+                            try {
+                                String str = new String(responseBody);
+                                JSONObject jsonObject = new JSONObject(str);
+                                if (jsonObject != null) {
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody, Throwable error) {
+
+                        if (responseBody != null) {
+
+
+                            String str = new String(responseBody);
+                            System.out.print(str);
+                        }
+                    }
+
+
+                    @Override
+                    public void onFinish() {
+                        super.onFinish();
+                        //closeLoadDialog();
+                    }
+
+
+                });
+
+
+                Intent intent1;
+                if ("1".equals(ed_account.getText().toString())) {
+
+                    intent1 = new Intent(LoginActivity.this, MainActivity.class);
+                } else {
+
+                    intent1 = new Intent(LoginActivity.this, SubMainActivity.class);
                 }
 
-                @Override
-                public void onError(Throwable ex, boolean isOnCallback) {
-                    Toast.makeText(x.app(), ex.getMessage(), Toast.LENGTH_LONG).show();
-                }
+                startActivity(intent1);
 
-                @Override
-                public void onCancelled(CancelledException cex) {
-                    Toast.makeText(x.app(), "cancelled", Toast.LENGTH_LONG).show();
-                }
+                break;
 
-                @Override
-                public void onFinished() {
-                   // Toast.makeText(x.app(), "cancelled", Toast.LENGTH_LONG).show();
-                }
-            });*/
-
-           /**/
-            Intent intent1;
-            if("1".equals(ed_account.getText().toString())){
-
-                 intent1 = new Intent(LoginActivity.this, MainActivity.class);
-            }else{
-
-                intent1 = new Intent(LoginActivity.this, SubMainActivity.class);
-            }
-
-          startActivity(intent1);
-
-          break;
-
-        //注册按钮
-       case  R.id.tv_resiger:
+            //注册按钮
+            case R.id.tv_resiger:
 
 
-          Intent intent2 = new Intent(LoginActivity.this, RegisterActivity.class);
-          startActivity(intent2);
+                Intent intent2 = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent2);
 
-          break;
+                break;
 
-        //忘记密码
-        case  R.id.tv_forgetpassword:
+            //忘记密码
+            case R.id.tv_forgetpassword:
 
-          Intent intent3 = new Intent(LoginActivity.this, PwdForgetActivity.class);
-          startActivity(intent3);
+                Intent intent3 = new Intent(LoginActivity.this, PwdForgetActivity.class);
+                startActivity(intent3);
 
-          break;
-        default:
-          break;
+                break;
+            default:
+                break;
 
 
-      }
+        }
     }
 }
