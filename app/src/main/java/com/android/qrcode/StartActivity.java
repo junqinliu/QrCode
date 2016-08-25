512d817dd37914f7e7f63409c81764d29ad1eab5
junqinliu <275063613@qq.com>
2016-08-25 20:14:42 +0800

    修改bug

--- app/src/main/java/com/android/qrcode/StartActivity.java
+++ app/src/main/java/com/android/qrcode/StartActivity.java
@@ -6,7 +6,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+
+import com.alibaba.fastjson.JSON;
 import com.android.application.ExitApplication;
+import com.android.mylibrary.model.UserInfoBean;
+import com.android.utils.SharedPreferenceUtil;
+import com.android.utils.TextUtil;
+
 public class StartActivity extends Activity implements View.OnClickListener {
 
     private static final int GO_HOME = 1000;
@@ -18,10 +24,39 @@ public class StartActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-       // 使用Handler的postDelayed方法，1.5秒后执行跳转到MainActivity
-        mHandler.sendEmptyMessageDelayed(GO_HOME, SPLASH_DELAY_MILLIS);
-        ExitApplication.getInstance().addAllActivity(this);
+
+        /**
+         * 跳过启动页
+         */
+        if (!TextUtil.isEmpty(SharedPreferenceUtil.getInstance(this).getSharedPreferences().getString("UserInfo", ""))) {
+
+            UserInfoBean userInfoBean = JSON.parseObject(SharedPreferenceUtil.getInstance(this).getSharedPreferences().getString("UserInfo", ""), UserInfoBean.class);
+            if (userInfoBean != null) {
+
+                Intent intent =  new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+
+            }else{
+
+                setContentView(R.layout.activity_splash);
+                // 使用Handler的postDelayed方法，1.5秒后执行跳转到MainActivity
+                mHandler.sendEmptyMessageDelayed(GO_HOME, SPLASH_DELAY_MILLIS);
+                ExitApplication.getInstance().addAllActivity(this);
+            }
+        }else{
+
+            setContentView(R.layout.activity_splash);
+            // 使用Handler的postDelayed方法，1.5秒后执行跳转到MainActivity
+            mHandler.sendEmptyMessageDelayed(GO_HOME, SPLASH_DELAY_MILLIS);
+            ExitApplication.getInstance().addAllActivity(this);
+        }
+
+
+
+
+
     }
 
 
